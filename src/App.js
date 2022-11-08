1f3b1b4daaa9f5a22f5eb86db6b2475125dbc85f
github_Ryan30551 <patrickryanf@gmail.com>
2022-11-07 22:43:39 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+//Hooks that allow us to track states in a function//
+import 'bootstrap/dist/css/bootstrap.min.css';
+//Imports styling for bootstrap//
 import './App.css';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchField from './components/SearchField';
+import Favorites from './components/Favorites';
 
-function App() {
+
+  //App with API from omdbapi.com//
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+  //Functions that pull Movie information//
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const getMovieRequest = async () => {
+
+  //API uses 'Search Value' to find any movie title typed in//
+  const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=bcc89972`;
+
+  const response = await fetch(url);
+  const responseJson = await response.json();
+
+  //returns value of movies searched in search field//
+
+  if (responseJson.Search) {
+    setMovies(responseJson.Search);
+  }
+};
+
+  //Local array used to store movies//
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [getMovieRequest, searchValue]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid movie-app'>
+       <div className='row d-flex align-items-center mt-4 mb-4'>
+       <MovieListHeading heading='Movies' />
+       <SearchField searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
+      <div className='row'>
+        <MovieList movies={movies} favoriteComponent= {Favorites} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
